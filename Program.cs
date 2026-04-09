ada136ea6e928d61cf430fb20fd82f819a49d35b
ishdryhn <ishadireyhan17@gmail.com>
2026-04-09 08:34:24 +0700

    adding var and method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 25.61.0283 Muhammad Ishadireyhan Putra
+
+int a = 10, b = 11;
+
+static int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+static int pengurangan(int a, int b)
+{
+    return a - b;
+}
+static int perkalian(int a, int b)
+{
+    return a * b;
+}
+static int pembagian(int a, int b)
+{
+    return a / b;
+}
+
+Console.WriteLine(a+" + "+b+" = "+penjumlahan(a,b));
+Console.WriteLine(a+" - "+b+" = "+pengurangan(a,b));
+Console.WriteLine(a+" * "+b+" = "+perkalian(a,b));
+Console.WriteLine(a+" / "+b+" = "+pembagian(a,b));
